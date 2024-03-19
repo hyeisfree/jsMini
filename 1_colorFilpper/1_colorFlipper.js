5154bb541fd195fd07a59498b0f5983f9b7b5f94
hyeisfree <nahye6559@naver.com>
2024-03-20 00:48:16 +0900

    자스 끝, css 도전할 것

--- 1_colorFilpper/1_colorFlipper.js
+++ 1_colorFilpper/1_colorFlipper.js
@@ -1,6 +1,10 @@
-let bgColor = [{ color: "purple" }, { color: "blue" }, { color: "yellow" }];
 let btn = document.querySelector("button");
 let color = document.getElementById("color");
+let background = document.getElementsByClassName("bg");
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+} // 구글링 -> Math.floor, Math.random 함수 찾아보기
 
 /* btn.addEventListener("click", function () {
    if (color.innerText === "red") {
@@ -9,7 +13,6 @@ let color = document.getElementById("color");
  }); */
 
 btn.addEventListener("click", () => {
-  if (color.innerText === "red") {
-    color.innerText = "purple";
-  }
-}); // 화살표 함수로 바꿔보기
+  color.innerText = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
+}); // 화살표 함수로 바꿈.
