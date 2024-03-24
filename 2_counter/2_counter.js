30a7aa9431d0d1c6ec3c9f21bd54776b8c9f9b4f
hyeisfree <nahye6559@naver.com>
2024-03-24 23:13:51 +0900

    counter html 수정 및 js 도전중

---
+++ 2_counter/2_counter.js
@@ -0,0 +1,4 @@
+let num = document.querySelector("article");
+const dec = document.querySelector("button");
+const res = document.querySelector("button");
+const inc = document.querySelector("button"); // DOM 객체 불러오기
