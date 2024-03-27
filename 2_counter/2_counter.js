0f9d155edbdf796e25124142669d993d63c39816
hyeisfree <nahye6559@naver.com>
2024-03-27 18:07:15 +0900

    아직 js 도전 중

--- 2_counter/2_counter.js
+++ 2_counter/2_counter.js
@@ -1,4 +1,19 @@
-let num = document.querySelector("article");
-const dec = document.querySelector("button");
-const res = document.querySelector("button");
-const inc = document.querySelector("button"); // DOM 객체 불러오기
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+//forEach, e, e.currentTarget, e.currentTarget.classList
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    num.textContent = count;
+  });
+});
