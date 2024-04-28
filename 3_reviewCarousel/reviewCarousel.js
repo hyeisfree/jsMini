06e9705d2eaf569be4240d55b69745e1c98ba4a9
hyeisfree <nahye6559@naver.com>
2024-04-28 20:23:41 +0900

    리뷰 캐러셀 만들기

--- 3_reviewCarousel/reviewCarousel.js
+++ 3_reviewCarousel/reviewCarousel.js
@@ -1,3 +1,5 @@
 const fe = document.querySelector(".fe");
 const be = document.querySelector(".be");
 const fu = document.querySelector(".fu");
+const btn_arrow = document.querySelector(".btn_arrow");
+const surprise = document.querySelector(".surprise");
