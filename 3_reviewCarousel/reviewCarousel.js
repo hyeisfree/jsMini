f05bd6ca4b439d7c3172e19694d72b568289cd54
hyeisfree <nahye6559@naver.com>
2024-04-30 16:42:58 +0900

    리뷰캐러셀 자바스크립트 진행 중

--- 3_reviewCarousel/reviewCarousel.js
+++ 3_reviewCarousel/reviewCarousel.js
@@ -3,3 +3,14 @@ const be = document.querySelector(".be");
 const fu = document.querySelector(".fu");
 const btn_arrow = document.querySelector(".btn_arrow");
 const surprise = document.querySelector(".surprise");
+
+let user = [
+  {
+    name: "김나혜",
+    job: "프론트엔드 개발자",
+  },
+  {
+    name: "류동하",
+    job: "백엔드 개발자",
+  },
+];
