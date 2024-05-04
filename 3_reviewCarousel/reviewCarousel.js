f24b96458981a06971487aca748f46952e959f3f
hyeisfree <nahye6559@naver.com>
2024-05-04 21:54:15 +0900

    리뷰 캐러셀

--- 3_reviewCarousel/reviewCarousel.js
+++ 3_reviewCarousel/reviewCarousel.js
@@ -4,19 +4,17 @@ const fu = document.querySelector(".fu");
 const btn_arrow = document.querySelector(".btn_arrow");
 const surprise = document.querySelector(".surprise");
 
-let user = [
+let userJob = [
   {
-    name: "김나혜",
     job: "프론트엔드 개발자",
   },
   {
-    name: "류동하",
     job: "백엔드 개발자",
   },
   {
-    name: "김멋사",
     job: "풀스택 개발자",
   },
 ];
 
-userName = prompt("이름을 입력해주세요!");
+let userName = prompt("이름을 입력해주세요!");
+alert("환영합니다, " + userName + "님");
